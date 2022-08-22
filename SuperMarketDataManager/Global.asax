fa329336da4a92759e023e4c9b30daac8782d1a0
priyan272 <priyan272@gmail.com>
2022-08-22 23:11:09 +1000

    Added Data Centers

---
+++ SuperMarketDataManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuperMarketDataManager.WebApiApplication" Language="C#" %>
---
+++ SuperMarketDataManager/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SuperMarketDataManager.Startup))]
+
+namespace SuperMarketDataManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
